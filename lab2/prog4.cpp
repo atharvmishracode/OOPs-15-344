a954d3f22546a698b17845eceec28a86f63c8e93
atharvmishracode <atharv666mishra@gmail.com>
2026-09-25 16:31:31 +0530

    Add lab2 program 4

---
+++ lab2/prog4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void callByValue(int x)
+{
+    x = x + 10;
+    cout << "Inside Call by Value: " << x << endl;
+}
+
+void callByReference(int &x)
+{
+    x = x + 10;
+    cout << "Inside Call by Reference: " << x << endl;
+}
+
+void callByAddress(int *x)
+{
+    *x = *x + 10;
+    cout << "Inside Call by Address: " << *x << endl;
+}
+
+int main()
+{
+    int a = 10, b = 10, c = 10;
+
+    callByValue(a);
+    cout << "After Call by Value: " << a << endl;
+
+    callByReference(b);
+    cout << "After Call by Reference: " << b << endl;
+
+    callByAddress(&c);
+    cout << "After Call by Address: " << c << endl;
+
+    return 0;
+}
